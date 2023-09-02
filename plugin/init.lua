0e19dd13a3751d2e00285917c5620eb79f470ac5
Luckas <luckasranarison@gmail.com>
2023-09-02 05:13:34 +0300

    feat(plugin): commentstring
    fix #4

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,8 @@
 vim.filetype.add({
-	pattern = { ["~/.config/hypr/.*%.conf"] = "hypr" },
+  pattern = { ["~/.config/hypr/.*%.conf"] = "hypr" },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "hypr",
+  callback = function(event) vim.bo[event.buf].commentstring = "# %s" end,
 })
