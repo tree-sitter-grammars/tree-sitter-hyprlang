24ca7b17177a7f3498bc4fae8a04ec922e5081ff
matt1432 <matt@nelim.org>
2023-11-25 23:45:02 -0500

    build: add nix support

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+  flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+
+    src = ./.;
+    version = "v1.0.2";
+
+    hypr-grammar = pkgs.tree-sitter.buildGrammar {
+      language = "hypr";
+      inherit version src;
+      generate = true;
+    };
+
+  in {
+    packages = {
+      default = pkgs.vimUtils.buildVimPlugin {
+        pname = "tree-sitter-hypr";
+        inherit version src;
+
+        # Since this package isn't with all the other treesitter grammars,
+        # we can simply add this line in the plugin to load it
+        preInstall = let
+          tsInstall = ''
+            vim.treesitter.language.require_language(\"hypr\", \"${hypr-grammar}/parser\")
+          '';
+        in ''
+          echo "${tsInstall}" >> ./plugin/init.lua
+        '';
+      };
+    };
+  });
+}
