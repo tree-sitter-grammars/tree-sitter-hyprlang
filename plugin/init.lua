e0b6170feddf0a7544a4f5649ad4458b3098d637
aserowy <serowy@hotmail.com>
2024-01-03 10:40:50 +0100

    added parser init for treesitter to nvim plugin
    Signed-off-by: aserowy <serowy@hotmail.com>

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "hypr",
   callback = function(event) vim.bo[event.buf].commentstring = "# %s" end,
 })
+
+require("nvim-treesitter.parsers").get_parser_configs().hypr = {
+    install_info = {
+        url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+        files = { "src/parser.c" },
+        branch = "master",
+    },
+    filetype = "hypr",
+}
