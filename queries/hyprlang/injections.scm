e5da7d0aa44403153e0394d87d9edea4e5bd6609
Luckas <luckasranarison@gmail.com>
2024-03-25 16:32:56 +0300

    feat(queries): more injections

--- queries/hyprlang/injections.scm
+++ queries/hyprlang/injections.scm
@@ -1,3 +1,22 @@
 (exec
   (string) @injection.content
   (#set! injection.language "bash"))
+
+(keyword
+  (name) @_name
+  (#any-of? @_name "windowrule" "windowrulev2")
+  (params
+    (string)
+    (string) @injection.content
+    (#set! injection.combined)
+    (#set! injection.language "regex")))
+
+(keyword
+  (name) @_name
+  (#match? @_name "bind(l|r|e|n|m|t|i)*")
+  (params
+    (_)+
+    (string) @_function
+    (#match? @_function ".*exec")
+    (string) @injection.content
+    (#set! injection.language "bash")))
