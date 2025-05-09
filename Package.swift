2d23887fc9a71b861cc3086cf7b00831f2c9edd0
ObserverOfTime <chronobserver@disroot.org>
2025-05-09 22:08:01 +0300

    update: tree-sitter (#27)
    * feat: update bindings
    
    * ci: add release workflow
    
    and clean up test workflow
    
    * feat: regenerate parser
    
    ---------
    
    Co-authored-by: luckasRanarison <luckasranarison@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 // swift-tools-version:5.3
+
+import Foundation
 import PackageDescription
+
+var sources = ["src/parser.c"]
+if FileManager.default.fileExists(atPath: "src/scanner.c") {
+    sources.append("src/scanner.c")
+}
 
 let package = Package(
     name: "TreeSitterHyprlang",
@@ -7,17 +14,14 @@ let package = Package(
         .library(name: "TreeSitterHyprlang", targets: ["TreeSitterHyprlang"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
     ],
     targets: [
         .target(
             name: "TreeSitterHyprlang",
             dependencies: [],
             path: ".",
-            sources: [
-                "src/parser.c",
-                // NOTE: if your language has an external scanner, add it here.
-            ],
+            sources: sources,
             resources: [
                 .copy("queries")
             ],
